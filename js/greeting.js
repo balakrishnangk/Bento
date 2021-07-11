5b46eadbb307e8fccd2c089383e92989a2550081
Bala Krishnan Gopala Krishnan <36544814+balakrishnangk@users.noreply.github.com>
2021-07-12 05:18:59 +0530

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -16,7 +16,7 @@ const gree6 = 'Good evening,  ';
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
   document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 12) {
+} else if (hour >= 5 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
   document.getElementById('greetings').innerText = gree3 + name;
